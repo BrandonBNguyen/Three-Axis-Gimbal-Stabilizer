7559a40532fbf2914273ec5199ae9e47ad42601b
Brandon Nguyen <BrandonBanhNguyen@gmail.com>
2021-02-27 21:03:03 -0800

    Update MPUTester.ino
    Successfully displayed heading angle from calibrated accelerometer/magnetometer.

--- Code/MPUTester/MPUTester.ino
+++ Code/MPUTester/MPUTester.ino
@@ -5,31 +5,27 @@
 const auto a_x_offset = -1.62163782;
 const auto a_y_offset =  1.07151603;
 const auto a_z_offset =  5.31442928;
-const auto m_x_offset =  6.259483335;
-const auto m_y_offset = -10.891534325;
-const auto m_z_offset = -9.070652965;
+const auto m_x_offset = -3.11650466;
+const auto m_y_offset = 12.11313533;
+const auto m_z_offset = 16.39547157;
+const auto m_x_scale = 0.91999950;
+const auto m_y_scale = 0.95515546;
+const auto m_z_scale = 1.15461063;
 
 struct Reading {
     // Calibrated values to be returned by the IMU.
-    const float a_x;
-    const float a_y;
-    const float a_z;
-    const float m_x;
-    const float m_y;
-    const float m_z;
     const float pitch_angle;
     const float roll_angle;
-//    const float heading_angle;
+    const float heading_angle;
     
     
-    Reading(const float a_x, const float a_y, const float a_z,
-        const float m_x, const float m_y, const float m_z,
-        const float pitch_angle, const float roll_angle)
-        : a_x{ a_x }, a_y{ a_y }, a_z{ a_z }, // Convert body-axis angles to degrees.
-          m_x{ m_x }, m_y{ m_y }, m_z{ m_z },
-        pitch_angle{ pitch_angle * 180 / M_PI }, roll_angle{ roll_angle * 180 / M_PI }
-//        heading_angle{ heading_angle * 180 / M_PI } {
-        {}
+    Reading(const float pitch_angle, const float roll_angle, const float heading_angle)
+        : pitch_angle{ pitch_angle * 180 / M_PI }, 
+          roll_angle{ roll_angle * 180 / M_PI }, 
+          heading_angle{ heading_angle * 180 / M_PI }
+    {
+    
+    }
 };
 
 struct CalibratedMPU : Mpu9250 {
@@ -41,19 +37,28 @@ struct CalibratedMPU : Mpu9250 {
         // This is aligned such that the +x axis labeled on the accelerometer corresponds to the
         // 1-body axis, the -y axis corresponds to the 2-body axis, and the -z axis corresponds
         // to the 3-body axis.
+
+        // Accelerometer Correction
         auto a_x =  accel_y_mps2() + a_x_offset;
         auto a_y = -accel_x_mps2() + a_y_offset;
         auto a_z = -accel_z_mps2() + a_z_offset;
-        auto pitch_angle = atan2(a_x, sqrt(pow(a_y, 2) + pow(a_z, 2)));
-        auto roll_angle = atan2(-a_y, a_z);
+
+        // Magnetometer Correction
+        // Correct Orientation
         auto m_x =  mag_y_ut();
         auto m_y = -mag_x_ut();
         auto m_z =  mag_z_ut();
+        // Correct offsets and scaling
+        m_x = (m_x - m_x_offset) * m_x_scale;
+        m_y = (m_y - m_y_offset) * m_y_scale;
+        m_z = (m_z - m_z_offset) * m_z_scale;
+
+        // Pitch, Roll, and Heading Angle Calculations
+        auto pitch_angle = atan2(a_x, sqrt(pow(a_y, 2) + pow(a_z, 2)));
+        auto roll_angle = atan2(-a_y, a_z);
         auto heading_angle = atan2(m_z * sin(roll_angle) - m_y * cos(roll_angle), 
             m_x*cos(pitch_angle) + m_y*sin(pitch_angle)*sin(roll_angle) + m_z*sin(pitch_angle)*cos(roll_angle) );
-        return Reading{ a_x, a_y, a_z,
-                        m_x, m_y, m_z,
-                        pitch_angle, roll_angle };
+        return Reading{ pitch_angle, roll_angle, heading_angle };
     }
 
 };
@@ -83,14 +88,11 @@ void loop() {
 //        Serial.print(reading.a_y, 6);
 //        Serial.print(" ");
 //        Serial.println(reading.a_z, 6);
-//        Serial.print(reading.pitch_angle, 6);
-//        Serial.print(" ");
-//        Serial.println(reading.roll_angle, 6);
-        Serial.print(reading.m_x, 6);
+        Serial.print(reading.pitch_angle, 6);
         Serial.print(" ");
-        Serial.print(reading.m_y, 6);
+        Serial.print(reading.roll_angle, 6);
         Serial.print(" ");
-        Serial.println(reading.m_z, 6);
+        Serial.println(reading.heading_angle, 6);
     }
     delay(100);
 }
